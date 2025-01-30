b008cd0ab3f3068a14ac9b538e8df11806aeca1b
Taher <tmttinwala8252@gmail.com>
2025-01-30 14:53:08 +0530

    msg dlt

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -5,7 +5,6 @@ const Login = () => {
         <div className="loginBackground">
             <div className="loginContainer">
                 <h1 className='loginH1'>Login</h1>
-                <p>hii</p>
                 <form>
                     <label>Email</label>
                     <input type="email" id="email" placeholder="Enter your email" required />
