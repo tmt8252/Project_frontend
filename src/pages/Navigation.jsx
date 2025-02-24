3c3bebc75e4d08b72d362bd8d7bb8b802359bebe
hinal2804 <hinalsimariya@gmail.com>
2025-02-24 17:37:25 +0530

    browse

--- src/pages/Navigation.jsx
+++ src/pages/Navigation.jsx
@@ -24,14 +24,38 @@ const Navigation = () => {
                     <ul>
                         <li><NavLink to={'/'}><i className="fas fa-home"></i> Home</NavLink></li>
                         <li className="dropdown">
-                            <NavLink to={'/categories'}>
+                            <NavLink to={'/categories'} className="browse-button">
                                 <i className="fas fa-book"></i> Browse <i className="fas fa-chevron-down"></i>
                             </NavLink>
                             <div className="dropdown-content">
-                                <NavLink to="/fiction">Fiction</NavLink>
-                                <NavLink to="/non-fiction">Non-Fiction</NavLink>
-                                <NavLink to="/children">Children's</NavLink>
-                                <NavLink to="/academic">Academic</NavLink>
+                                <NavLink to="/fiction" className="dropdown-item">
+                                    <i className="fas fa-theater-masks"></i>
+                                    <div>
+                                        <span>Fiction</span>
+                                        <small>Novels, Stories & More</small>
+                                    </div>
+                                </NavLink>
+                                <NavLink to="/non-fiction" className="dropdown-item">
+                                    <i className="fas fa-brain"></i>
+                                    <div>
+                                        <span>Non-Fiction</span>
+                                        <small>Biography, Self-Help & More</small>
+                                    </div>
+                                </NavLink>
+                                <NavLink to="/children" className="dropdown-item">
+                                    <i className="fas fa-child"></i>
+                                    <div>
+                                        <span>Children's</span>
+                                        <small>Picture Books & Stories</small>
+                                    </div>
+                                </NavLink>
+                                <NavLink to="/academic" className="dropdown-item">
+                                    <i className="fas fa-graduation-cap"></i>
+                                    <div>
+                                        <span>Academic</span>
+                                        <small>Textbooks & Study Materials</small>
+                                    </div>
+                                </NavLink>
                             </div>
                         </li>
                         <li>
